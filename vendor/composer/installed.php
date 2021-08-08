251450d4cb3699529154b633829511ca93033c22
Hasan Muhammad <43256553+HasanMu@users.noreply.github.com>
2021-08-08 14:06:21 +0700

    [FIX] composer.json & composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '932bb24d3cb78bced81814bee152a676971775e1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '932bb24d3cb78bced81814bee152a676971775e1',
+    ),
+  ),
+);
